b62b5ffcd05b8d9513b976383f27380db8ff2b43
hn-88 <hn-88@users.noreply.github.com>
2018-05-14 14:28:18 +0530

    adding matlab visualization files

---
+++ Matlabfiles/imgabs.m
@@ -0,0 +1,31 @@
+figure;
+indexi=1;
+while(1)
+    S=imread(sprintf('res%03d.png', indexi));
+    imagesc(S);colorbar;
+    title(sprintf('slice %d', indexi));
+    
+    k = waitforbuttonpress;
+    % 28 leftarrow
+    % 29 rightarrow
+    % 30 uparrow
+    % 31 downarrow
+    x = double(get(gcf,'CurrentCharacter'))
+    if (x=='q')
+      break
+    end
+    
+    if (x=='n')
+     indexi=indexi+1;
+     if indexi>100
+      indexi=1;
+     end
+    end
+    
+    if (x=='p')
+     indexi=indexi-1;
+     if indexi<1
+      indexi=100;
+     end
+    end
+end
--- Matlabfiles/imgscv.m
+++ Matlabfiles/imgscv.m
@@ -1,1 +1,25 @@
-
+figure;
+indexi=1;
+while(1)
+    imagesc(V(:,:,indexi));colorbar;
+    title(sprintf('slice %d', indexi));
+    
+    x=kbhit();
+    if (x=='q')
+      break
+    end
+    
+    if (x=='n')
+     indexi++;
+     if indexi>148
+      indexi=1;
+     end
+    end
+    
+    if (x=='p')
+     indexi--;
+     if indexi<1
+      indexi=148;
+     end
+    end
+end
