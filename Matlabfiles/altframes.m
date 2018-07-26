5958a714270a43400cab02684bf9f6f86e75e6f9
hn-88 <hn-88@users.noreply.github.com>
2018-07-26 14:34:32 +0530

    altframes when not averaging

--- Matlabfiles/altframes.m
+++ Matlabfiles/altframes.m
@@ -1,10 +1,10 @@
 pkg load signal;
 
-for k = 1:19
-	for p = 0:9	
+for k = 1:99
+	%for p = 0:9	
 	 
-	dataFileName = strcat('slice', num2str(k,'%03d'),'-',num2str(p,'%03d'), '.png');
-    backgroundFileName = strcat('slice', num2str(k+1,'%03d'),'-',num2str(p,'%03d'), '.png');    
+	dataFileName = strcat('slice', num2str(k,'%03d'), '.png');
+    backgroundFileName = strcat('slice', num2str(k+1,'%03d'), '.png');    
 	if exist(dataFileName, 'file')
 		a = double(imread(dataFileName));
         background = double(imread(backgroundFileName));
@@ -12,12 +12,12 @@ for k = 1:19
         res=abs(hilbert(b));
 										  
       
-        outFileName = strcat('altframeres_', num2str(k,'%03d'),'-',num2str(p,'%03d'), '.png');
+        outFileName = strcat('altframeres_', num2str(k,'%03d'), '.png');
         imwrite(uint8(res*5),outFileName,'png');
 	else
 		fprintf('File %s does not exist.\n', dataFileName);
 	end
 	
-	end
+	%end
 		
 end
